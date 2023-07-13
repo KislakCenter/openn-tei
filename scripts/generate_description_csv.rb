625fb6a0f4f51ac3d7b8609d70f44bd34b9af1e9
Doug Emery <emeryr@upenn.edu>
2023-07-12 20:35:48 -0400

    Accept piped input; reformat language codes

--- scripts/generate_description_csv.rb
+++ scripts/generate_description_csv.rb
@@ -12,9 +12,11 @@ LANGUAGE_CODES_XPATH    = '/TEI/teiHeader/fileDesc/sourceDesc/msDesc/msContents/
 DATE_XPATH              = '/TEI/teiHeader/fileDesc/sourceDesc/msDesc/history/origin/origDate'
 PLACE_XPATH             = '/TEI/teiHeader/fileDesc/sourceDesc/msDesc/history/origin/origPlace'
 
-def get_values xml, xpath
-  xml.xpath(xpath).filter_map { |s| s.text unless s.text.strip.empty? }.join('|')
+def get_values xml, xpath, separator: '|'
+  xml.xpath(xpath).filter_map { |s| s.text unless s.text.strip.empty? }.join(separator)
 end
+
+tei_files = STDIN.tty? ? ARGV : ARGF
 
 headers = %w{
   shelfmark
@@ -26,11 +28,10 @@ headers = %w{
   date
   place
 }
-
 CSV headers: true do |csv|
   csv << headers
-  ARGV.each do |file|
-    xml = File.open (file) { |f| Nokogiri::XML f }
+  tei_files.each do |file|
+    xml = File.open (file.chomp) { |f| Nokogiri::XML f }
     xml.remove_namespaces!
 
     row = {}
@@ -40,7 +41,7 @@ CSV headers: true do |csv|
     additional_titles = get_values(xml, ADDITIONAL_TITLES_XPATH)
     authors           = get_values(xml, AUTHORS_XPATH)
     language          = get_values(xml, LANGUAGE_XPATH)
-    language_codes    = get_values(xml, LANGUAGE_CODES_XPATH)
+    language_codes    = get_values(xml, LANGUAGE_CODES_XPATH).gsub(' ', '|')
     date              = get_values(xml, DATE_XPATH)
     place             = get_values(xml, PLACE_XPATH)
 
