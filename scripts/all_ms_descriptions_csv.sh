b51b833414466c95c4d8d204ea0df99b382b605f
Doug Emery <emeryr@upenn.edu>
2023-07-12 21:01:56 -0400

    Print name of output file

--- scripts/all_ms_descriptions_csv.sh
+++ scripts/all_ms_descriptions_csv.sh
@@ -2,4 +2,7 @@
 
 this_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-find ${this_dir}/../Data -iname \*tei.xml | ruby ${this_dir}/generate_description_csv.rb > all_ms_descriptions.csv
+output=all_ms_descriptions.csv
+find ${this_dir}/../Data -iname \*tei.xml | ruby ${this_dir}/generate_description_csv.rb > all_ms_descriptions.csv > ${output}
+
+echo "Wrote: ${output}"
